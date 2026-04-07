c11460c8818fcb288d1d58a951b643f682ccc8f8
Eloic Rakoto-Arisoa <eloicmax@gmail.com>
2026-04-07 10:33:44 +0300

    Commit data sql

---
+++ bd/Exemple - Importation manuelle Manuelle.sql
@@ -0,0 +1,8 @@
+
+
+Insert into MVT_BONS_VALIDE   (D_BONS, NUMSEM, TXMP) Values   ('27/03/26', 1, 4.42);
+Insert into MVT_BONS_VALIDE   (D_BONS, NUMSEM, TXMP) Values   ('27/03/26', 2, 5.41);
+Insert into MVT_BONS_VALIDE   (D_BONS, NUMSEM, TXMP) Values   ('27/03/26', 3, 6.57);
+Insert into MVT_BONS_VALIDE   (D_BONS, NUMSEM, TXMP) Values   ('27/03/26', 4, 8.68);
+Insert into MVT_BONS_VALIDE   (D_BONS, NUMSEM, TXMP) Values   ('27/03/26', 5, 10.36);
+Insert into MVT_BONS_VALIDE   (D_BONS, NUMSEM, MAN, MAN1, MAN2,     MSM1, MAD1) Values   ('27/03/26', 6, 44, 44, 0, 147.24, 44);
